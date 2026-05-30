9c3de635267ea8658a3122dc6ae3bf10c27674ff
Igor Alcântara Proença Leiteo <igorninjaleite@gmail.com>
2026-05-30 19:13:56 -0400

    product-management-system

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         ProdutoService service = new ProdutoService();
 
+
         // Adicionando produto
         Produto p1 = new Produto(1L, "Teclado Mecânico", 250.0, 10);
         service.adicionarProdutos(p1);
