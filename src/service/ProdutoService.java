226e0b4a155aa7ace2bf2234231012383fa77e00
Igor Alcântara Proença Leiteo <igorninjaleite@gmail.com>
2026-06-22 18:16:25 -0400

    Modificação Service com Exception

--- src/service/ProdutoService.java
+++ src/service/ProdutoService.java
@@ -1,5 +1,7 @@
 package service;
 
+import exception.PrecoInvalidoException;
+import exception.ProdutoNaoEncontradoException;
 import model.Produto;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +13,27 @@ public class ProdutoService {
 
     //Adicionar Produtos
     public void adicionarProdutos(Produto produto){
+        if (produto.getId() <= 0) {
+            throw new IdInvalidoException("ID deve ser maior que zero. " );
+        }
+
+        if (produto.getPreco() <= 0) {
+            throw new PrecoInvalidoException("Preço não pode ser negativo.");
+        }
+
+        if (produto.getQuantidadesEstoque() < 0) {
+            throw new ProdutoNaoEncontradoException(
+                    "Quantidade em estoque não pode ser negativa.");
+        }
+
         produtos.add(produto);
     }
 
     //Listar Produtos
     public void listarProdutos() {
+
         for (Produto produto : produtos) {
+
             System.out.println( "ID:" +
             produto.getId() + " - " + produto.getNome() + " - R$" + produto.getPreco()
             );
@@ -28,23 +45,30 @@ public class ProdutoService {
         produtos.remove(produto);
     }
 
+    //Atualizar preço
     public void atualizarPreco(Long id, double preco){
-        Produto produtoEncontrado = buscarProdutoPorID(id);
 
-        if (produtoEncontrado != null){
-            produtoEncontrado.setPreco(preco);
-        }else {
-            System.out.println("Produto não encontrado.");
+        Produto produtoEncontrado = buscarProdutoPorID(id);
+        if (preco < 0) {
+            throw new PrecoInvalidoException("Preço não pode ser negativo.");
         }
+        produtoEncontrado.setPreco(preco);
     }
     //Buscar por ID
     public Produto buscarProdutoPorID(Long id){
+        //Se for menor que zero:
+        if (id <= 0 ) {
+            throw new IdInvalidoException("ID deve ser maior que zero. " );
+        }
+
+        //Manipulação do produto
         for (Produto produto : produtos) {
             if (produto.getId().equals(id)){
                 return produto;
             }
         }
-        return null;
+        //Exception se o id não for correto
+        throw new ProdutoNaoEncontradoException("Produto de ID: " + id + " Não encontrado. ");
     }
 }
 
