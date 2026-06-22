c52d2c83f820018b60fec44e2b096ba293bc8f59
Igor Alcântara Proença Leiteo <igorninjaleite@gmail.com>
2026-06-22 18:16:51 -0400

    Testes dos Exception no Mais.

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,16 @@ public class Main {
         // Adicionando produto
         Produto p1 = new Produto(1L, "Teclado Mecânico", 250.0, 10);
         Produto p2 = new Produto(2L, "Mouse", 250.00, 2);
+        Produto p3 = new Produto(-2L,"Teste", -250.5, -2);
         service.adicionarProdutos(p1);
         service.adicionarProdutos(p2);
+        service.adicionarProdutos(p3);
+
 
         // Listando
         service.listarProdutos();
+        service.buscarProdutoPorID(-2L);
         service.buscarProdutoPorID(2L);
+        service.atualizarPreco(1L, 10);
     }
 }
